16d2049d9f0bfbea08408ed65579082f94698e6f
Devendra Bangar <devendrabangar8.com>
2024-03-18 22:54:00 +0530

    [IBA_to_ECOM] Changed IBA app into Ecom App.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    user = get_current_user(session[:user_id])
-  end
-
   private   
-  def get_current_user(id)
-    id.present? ? User.find(id) : nil   
+  def current_user   
+    @current_user = session[:user_id].present? ? User.find(session[:user_id]) : nil   
   end   
   helper_method :current_user   
 end
